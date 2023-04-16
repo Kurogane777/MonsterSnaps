3780794b955fa78d8891eff2577f85765cee203b
Kurogane Kroz <krozflame@gmail.com>
2023-04-15 22:10:46 -0700

    Menu Added

--- Assets/Scripts/Scripts/Character/Monster/MonsterPatrol.cs
+++ Assets/Scripts/Scripts/Character/Monster/MonsterPatrol.cs
@@ -6,9 +6,12 @@ using System.Collections;
 public class MonsterPatrol : MonoBehaviour
 {
 
-    public Transform[] points;
+    //public Transform[] points;
+    public PatrolList pList;
     private int destPoint = 0;
     private NavMeshAgent agent;
+
+    public bool LOL;
 
 
     void Start()
@@ -20,30 +23,60 @@ public class MonsterPatrol : MonoBehaviour
         // approaches a destination point).
         agent.autoBraking = false;
 
-        GotoNextPoint();
+        if (pList != null)
+        {
+            GotoNextPoint();
+        }
     }
 
 
     void GotoNextPoint()
     {
         // Returns if no points have been set up
-        if (points.Length == 0)
+        if (pList.points.Length == 0)
             return;
 
         // Set the agent to go to the currently selected destination.
-        agent.destination = points[destPoint].position;
+        agent.destination = pList.points[destPoint].position;
 
         // Choose the next point in the array as the destination,
         // cycling to the start if necessary.
-        destPoint = (destPoint + 1) % points.Length;
+        destPoint = (destPoint + 1) % pList.points.Length;
+    }
+
+    void NearestPoint()
+    {
+        //GameObject[] pListArea = GameObject.FindObjectOfType<PatrolList>();
+        GameObject[] pListArea = GameObject.FindGameObjectsWithTag("A");
+        if (pListArea.Length > 0)
+        {
+            GameObject closestPArea = pListArea[0];
+            foreach (GameObject plistA in pListArea)
+            {
+                if (Vector3.Distance(plistA.transform.position, transform.position) < Vector3.Distance(closestPArea.transform.position, transform.position))
+                {
+                    closestPArea = plistA;
+                }
+            }
+            pList = closestPArea.gameObject.GetComponent<PatrolList>();
+        }
     }
 
 
     void Update()
     {
+        if (!enabled) { pList = null; LOL = false; }
+
+        NearestPoint();
+
         // Choose the next destination point when the agent gets
         // close to the current one.
         if (!agent.pathPending && agent.remainingDistance < 0.5f)
-            GotoNextPoint();
+        {
+            if (pList != null)
+            {
+                GotoNextPoint();
+            }
+        }
     }
 }
